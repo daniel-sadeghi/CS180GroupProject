80a0c6478cd606feacb3b956481c337859bc5ea5
aguin005 <aguin005@ucr.edu>
2024-02-26 10:47:27 -0800

    Mock profile view

--- recipe-expo-app/screens/ProfileView.js
+++ recipe-expo-app/screens/ProfileView.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 function ProfileView({ navigation }) {
   return (
-    <View>
-      <Text>User Profile Screen</Text>
+    <View style={styles.container}>
       {/* Display user profile information here */}
+      <Image
+        source={require('../assets/profile.jpeg')}
+        style={styles.profilePicture}
+      />
+      <Text style={styles.name}>{"John Doe"}</Text>
+      {/* Have the users name above*/}
+      <Text style={styles.email}>{"JohnDoe@email.com"}</Text> 
+      {/* Have the users email above*/}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    position: 'absolute' ,top: -200, left: 0, right: 0, bottom: 200,
+    alignItems: 'center',
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  email: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
+
 
 export default ProfileView;
