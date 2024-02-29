e5ddbd35a9bb652fe48a6c372ad2576ece89b8db
AndrewValerio <andrewvalerio1396@gmail.com>
2024-02-29 15:37:15 -0800

    Updated searchview

--- recipe-expo-app/screens/SearchView.js
+++ recipe-expo-app/screens/SearchView.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, TextInput, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
+import Recipe from '../components/RecipeComponent'
 import {useNavigation} from '@react-navigation/native'
 
 function SearchView({ navigation }) {
@@ -9,9 +10,47 @@ function SearchView({ navigation }) {
              placeholder='Search Recipes'
             style={{borderWidth:1}}
             />
-
+            <Text style= {styles.text}>Results </Text>
+                
+                <Recipe
+                    imageSource={require('../assets/ratatouille.jpg')}
+                    bannerText="ratatouille"  
+                />
+                <Recipe
+                    imageSource={require('../assets/ratatouille.jpg')}
+                    bannerText="ratatouille"  
+                />
+                <Recipe
+                    imageSource={require('../assets/ratatouille.jpg')}
+                    bannerText="ratatouille"  
+                />
         </ScrollView>
   );
 }
 
 export default SearchView;
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    backgroundColor: '#90EE90',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  link: {
+    color: 'blue',
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  button: {
+    color: 'blue',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
