561b8cd7527425d0e93ce3a1854ba3ea902ea19e
aguin005 <aguin005@ucr.edu>
2024-02-29 14:40:42 -0800

    Photo picker working

--- recipe-expo-app/screens/ProfileView.js
+++ recipe-expo-app/screens/ProfileView.js
@@ -1,14 +1,59 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+// npx expo install expo-image-picker -- --force
+import Constants from 'expo-constants';
+// npx expo install expo-constants -- --force        
+import * as Permissions from 'expo-permissions';
+// npx expo install expo-permissions -- --force    
 
 function ProfileView({ navigation }) {
+  const [image, setImage] = useState(null);
+
+  const getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* Display user profile information here */}
-      <Image
+      {/* <Image
         source={require('../assets/profile.jpeg')}
+        
         style={styles.profilePicture}
+      /> */}
+      {console.log('Image URI:', image)}
+      {image && (
+      <Image 
+        source={{ uri: image }} 
+        style={styles.profilePicture} 
+        onLoad={() => {
+          console.log("Image loaded");
+        }}
+        onError={(error) => console.log('Image error', error)}
       />
+      )}
+      <Button title="Select Photo" onPress={pickImage} />
       <Text style={styles.name}>{"John Doe"}</Text>
       {/* Have the users name above*/}
       <Text style={styles.email}>{"JohnDoe@email.com"}</Text> 
