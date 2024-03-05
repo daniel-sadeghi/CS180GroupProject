e29dcf9ba70eca7b53cf48a2d39547339061b8d0
daniel-sadeghi <daniel.sadeghi@fastmail.com>
2024-03-05 15:50:42 -0800

    Added prices to ingredients and changed styling

--- recipe-expo-app/components/Ingredient.js
+++ recipe-expo-app/components/Ingredient.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {View, StyleSheet, Text, Image} from 'react-native';
+import USDFormat from '../utils/USDFormat';
 
 const Ingredient = ({name, price, image}) => {
+
     return (
-        <View style={styles.ingredient}>
-            <View style={styles.name}>
-                <Text style={styles.name}>{name}</Text>
-            </View>
+        <View style={styles.ingredientBox}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.price}>{USDFormat(price)}</Text>
             <View style={styles.image}>
                 <Image source={{uri: image}} resizeMode="stretch" />
             </View>
@@ -15,14 +16,19 @@ const Ingredient = ({name, price, image}) => {
 };
 
 const styles = StyleSheet.create({
-    ingredient: {
+    ingredientBox: {
         flexDirection: 'row',
-        backgroundColor: '#a4f28a',
-        padding: 20,
-        marginVertical: 3,
-        marginHorizontal: 16,
+        backgroundColor: '#f8f8ff',
+        padding: 15,
+        borderWidth: 1,
+        borderColor: '#C0C0C0',
+        marginHorizontal: 0,
     },
     name: {
+        flex: 5,
+        fontSize: 16,
+    },
+    price: {
         flex: 1,
         fontSize: 16,
     },
