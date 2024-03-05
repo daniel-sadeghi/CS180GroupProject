51af5f4250326559546b8a803bbec1904ce9e32e
daniel-sadeghi <daniel.sadeghi@fastmail.com>
2024-03-05 04:32:34 -0800

    Removed header for TabNavigator view

--- recipe-expo-app/App.js
+++ recipe-expo-app/App.js
@@ -11,7 +11,7 @@ const App = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="MainView" component={TabNavigator} />
+                <Stack.Screen name="MainView" component={TabNavigator} options={{headerShown: false}}/>
                 <Stack.Screen name="RecipeView" component={RecipeView}/>
             </Stack.Navigator>
         </NavigationContainer>
