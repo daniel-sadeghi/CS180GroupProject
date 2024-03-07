1646de2be89775b6992b7de2d6fc9303d17714db
daniel-sadeghi <daniel.sadeghi@fastmail.com>
2024-03-07 03:49:44 -0800

    Added an "Add to Cart" pop up alert

--- recipe-expo-app/components/AddToCartButton.js
+++ recipe-expo-app/components/AddToCartButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Pressable, Text, Animated} from 'react-native';
+import {Pressable, Text, Animated, Alert} from 'react-native';
 
 const animated = new Animated.Value(1);
 
-const AddToCartButton = ({children, style}) => {
+const AddToCartButton = ({children, style, navigation}) => {
     const fadeIn = () => {
         Animated.timing(animated, {
           toValue: 0.1,
@@ -12,16 +12,17 @@ const AddToCartButton = ({children, style}) => {
         }).start();
     };
 
-    const fadeOut = () => {
+    const onPressOut = () => {
         Animated.timing(animated, {
             toValue: 1,
             duration: 100,
             useNativeDriver: true,
         }).start();
+        Alert.alert("Added to Cart", "Ingredients have been successfully added to your cart");
     };
 
       return (
-        <Pressable onPressIn={fadeIn} onPressOut={fadeOut} style={style}>
+        <Pressable onPressIn={fadeIn} onPressOut={onPressOut} style={style}>
             <Animated.View style={{ opacity: animated }}>{children}</Animated.View>
         </Pressable>
       );
