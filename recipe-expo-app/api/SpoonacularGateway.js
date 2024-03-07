4c8c7f8915dfb90b215cb7523f23f1c45e656d44
daniel-sadeghi <daniel.sadeghi@fastmail.com>
2024-03-06 21:49:49 -0800

    WIP Caching API Fetches using AsyncStorage

--- recipe-expo-app/api/SpoonacularGateway.js
+++ recipe-expo-app/api/SpoonacularGateway.js
@@ -1,12 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const URI = 'https://api.spoonacular.com/recipes';
 const API_KEY = "0e05e31e1192449ab972630943bc0865" //TODO Fetch the API Key from the backend server
 
 const fetchSpoonData = async (endpoint, options=[]) => {
-    url = URI + `/${endpoint}?apiKey=${API_KEY}`;
+    let url = URI + `/${endpoint}?`;
     options.forEach(option => {
         url += `&${option}`;
     });
-    return await fetch(url);
-};
+    console.log('URL:', url);
+
+    const cachedResponse = await AsyncStorage.getItem(url);
+
+    if (cachedResponse !== null) {
+        return JSON.parse(cachedResponse);
+    } else {
+        const urlWithKey = url + `&apiKey=${API_KEY}`;
+        const getResponse = await fetch(urlWithKey).then(response => response.json());
+        await AsyncStorage.setItem(url, JSON.stringify(getResponse));
+        return getResponse;
+    }
+}
 
 export default fetchSpoonData;
